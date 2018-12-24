65f6a6bc77f35096f95e223f28b3081291193947
guoliang <guoliang0511@qq.com>
2018-12-24 23:24:49 +0800

    198. House Robber

---
+++ 0198-House-Robber/0198-House-Robber.js
@@ -0,0 +1,42 @@
+// 198. House Robber
+// https://leetcode.com/problems/house-robber/
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed,
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and
+it will automatically contact the police if two adjacent houses were broken into on the same night.
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of
+money you can rob tonight without alerting the police.
+
+Example 1:
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+  if (!nums || nums.length === 0) {
+    return 0;
+  }
+  if (nums.length === 1) {
+    return nums[0];
+  }
+  const dp = new Array(nums.length);//dp[index]表示考虑从nums[0...index]偷取财物的最大值
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < nums.length; ++i) {
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  return dp[dp.length - 1];
+};
+console.log(rob([2, 7, 9, 3, 1]));
+// Runtime: 52 ms, faster than 81.05% of JavaScript online submissions for House Robber.
