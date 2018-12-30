e417d1ce4d5b5fca27b6660e71e17e1d9b01272c
guoliang <guoliang0511@qq.com>
2018-12-30 14:01:22 +0800

    442. Find All Duplicates in an Array

---
+++ 0442-Find-All-Duplicates-in-an-Array/0442-Find-All-Duplicates-in-an-Array.js
@@ -0,0 +1,32 @@
+// 442. Find All Duplicates in an Array
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+/*
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+Find all the elements that appear twice in this array.
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+  let res = [];
+  for (let i = 0; i < nums.length; ++i) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] < 0) {
+      res.push(index + 1);
+    } else {
+      nums[index] *= -1;
+    }
+  }
+  return res;
+};
+console.log(findDuplicates([4, 3, 2, 7, 8, 2, 3, 1]));
+// Runtime: 104 ms, faster than 99.08% of JavaScript online submissions for Find All Duplicates in an Array.
