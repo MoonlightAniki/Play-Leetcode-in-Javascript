a1840844e135434bcddae7585d99d45b6690f455
guoliang <guoliang0511@qq.com>
2018-12-30 14:32:49 +0800

    406. Queue Reconstruction by Height

---
+++ 0406-Queue-Reconstruction-by-Height/0406-Queue-Reconstruction-by-Height.js
@@ -0,0 +1,42 @@
+// 406. Queue Reconstruction by Height
+// https://leetcode.com/problems/queue-reconstruction-by-height/
+/*
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where
+h is the height of the person and k is the number of people in front of this person who have a height greater than or equal
+to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+
+Example
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+ */
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function (people) {
+  people.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : b[0] - a[0]));
+  // console.log(people);
+  for (let i = 0; i < people.length; ++i) {
+    if (people[i][1] < i) {
+      moveTo(people, i, people[i][1]);
+    }
+  }
+  return people;
+};
+
+function moveTo(arr, src, dest) {
+  let val = arr[src];
+  for (let i = src - 1; i >= dest; --i) {
+    arr[i + 1] = arr[i];
+  }
+  arr[dest] = val;
+}
+
+const people = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]];
+console.log(reconstructQueue(people));
+// Runtime: 100 ms, faster than 50.35% of JavaScript online submissions for Queue Reconstruction by Height.
